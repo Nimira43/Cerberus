0a4e88b05185798e4ce7fdf82e783aee62871792
Nimira43 <n.rabone72@aol.com>
2025-02-20 06:10:56 +0000

    httpServer listening on port 3000

--- api/src/lib/server.js
+++ api/src/lib/server.js
@@ -4,7 +4,9 @@ export const startServer = () => {
   const httpServer = express()
 
   try {
-
+    httpServer.listen(3000, () => {
+      console.log(`Server running on Port 3000`)
+    })
   } catch (err) {
     throw new Error(err)
   }
