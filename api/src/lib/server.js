e1a507486f7bf8f1ff458eac6d6a4981414d5805
Nimira43 <n.rabone72@aol.com>
2025-02-20 06:21:21 +0000

    Port running from .env file.

--- api/src/lib/server.js
+++ api/src/lib/server.js
@@ -3,6 +3,8 @@ import express from 'express'
 export const startServer = () => {
   const httpServer = express()
 
+  console.log(`Port from ${process.env.PORT}`)
+
   try {
     httpServer.listen(3000, () => {
       console.log(`Server running on Port 3000`)
