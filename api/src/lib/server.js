d9003dc8ebdc5ff88a08590bf0956955ee86b69d
Nimira43 <n.rabone72@aol.com>
2025-03-23 10:40:04 +0000

    Cleaned up server.js

--- api/src/lib/server.js
+++ api/src/lib/server.js
@@ -6,9 +6,6 @@ export const startServer = () => {
   const port = config.port
 
   try {
-    console.log(`Cerberus image has been rebuilt`)
-    console.log(`This message is coming from inside the Cerberus image`)
-
     httpServer.listen(port, () => {
       console.log(`Server running on port ${port}`)
     })
